2af3b1fc181bd39587c937e6f7e9e695a9f933d8
Kim Marquardt <kim.marquardt@gmail.com>
2019-03-06 17:44:23 -0800

    Handle POST /sms

--- index.js
+++ index.js
@@ -3,13 +3,25 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const accountPhoneNumber = process.env.TWILIO_NUMBER;
+const port = process.env.APP_PORT;
 
+const app = require('express')();
 const client = require('twilio')(accountSid, authToken);
+const http = require('http');
 
 client.messages
   .create({
-         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-         from: '+15017122661',
-         to: accountPhoneNumber,
-       })
+           body: 'test message from the island',
+           from: '+1234567890',
+           to: accountPhoneNumber,
+         })
   .then(message => console.log(message.sid));
+
+app.post('/sms', (req, res) => {
+  console.log('request: ', req);
+  console.log('response: ', res);
+});
+
+http.createServer(app).listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
